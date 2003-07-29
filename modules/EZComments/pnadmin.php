926cc6b9736a0e5ee66107bb813926a9aecdc765
joerg <joerg@code.zikula.org>
2003-07-29 22:09:35 +0000

    Header comments

--- modules/EZComments/pnadmin.php
+++ modules/EZComments/pnadmin.php
@@ -1,22 +1,31 @@
 <?php
-// $Id$
-// ----------------------------------------------------------------------
-// EZComments
-// Attach comments to any module calling hooks
-// ----------------------------------------------------------------------
-// Author: Jörg Napp, http://postnuke.lottasophie.de
-// ----------------------------------------------------------------------
 // LICENSE
+// 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License (GPL)
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// For questions, help, comments, discussion, etc., please visit
+// the EZComments homepage http://lottasophie.sourceforge.net, 
+// the PostNuke homepage http://www.postnuke.com, or the German 
+// PostNuke Support page http://www.post-nuke.net
+//
+// @link http://lottasophie.sourceforge.net
+// @copyright 2001,2002 ispi of Lincoln, Inc.
+// @author Joerg Napp <jnapp@users.sourceforge.net>
+// @package EZComments
+// @version 0.2
+
 
 /**
  * Main administration function
