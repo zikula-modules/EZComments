cbac0ec736097908469f1ed33f85e82acf339e98
joerg <joerg@code.zikula.org>
2005-01-20 07:34:28 +0000

    Added Czech translations, credits to David Mach

---
+++ modules/EZComments/pnlang/ces/global.php
@@ -0,0 +1,70 @@
+<?php 
+// $Id$
+// ----------------------------------------------------------------------
+// EZComments
+// Attach comments to any module calling hooks
+// ----------------------------------------------------------------------
+// Translated by David Mach, watchingeye@users.sourceforge.net
+// ----------------------------------------------------------------------
+// LICENSE
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+
+define('_EZCOMMENTS', 		  			'Komentáøe');
+define('_EZCOMMENTS_NOAUTH',   			'Nemáte pøístup ke komentáøùm.');
+define('_EZCOMMENTS_ONLYREG',   		'Pouze pøihlášení uživatelé mohou komentovat.');
+define('_EZCOMMENTS_GOTOREG',   		'Registrovat/Pøihlásit');
+define('_EZCOMMENTS_ADD', 	  			'Pøidat');
+define('_EZCOMMENTS_DEL',      			'Smazat komentáø');
+define('_EZCOMMENTS_COMMENT_ADD', 		'Pøidat nový komentáø');
+define('_EZCOMMENTS_COMMENT_ANSWER', 	'Odpovìï');
+define('_EZCOMMENTS_COMMENT_FROM',  	'od');
+define('_EZCOMMENTS_COMMENT_ON',    	'pro');
+define('_EZCCOMMENTSCREATED',			'Komentáø byl pøidán');
+define('_EZCCOMMENTSDELETED', 			'Komentáø byl smazán');
+define('_EZCOMMENTS_FAILED',   			'Interní chyba');
+define('_EZCOMMENTS_NODIRECTACCESS',	'K tomuto modulu nelze pøistupovat pøímo');
+define('_EZCOMMENTS_RULES',	            'Definujte pravidla pro vaše komentáøe');
+
+define('_EZCOMMENTS_ADMIN',				'Administrace EZComments');
+define('_EZCOMMENTS_ADMIN_MAIN',		'Hlavní administrace EZComments');
+define('_EZCOMMENTS_SMARTYPATH',		'Cesta k vaší instalaci SMARTY');
+define('_EZCOMMENTS_SENDINFOMAIL',		'Odeslat mail pøi každém novém komentáøi');
+define('_EZCOMMENTS_OK', 				'Uložit');
+define('_EZCOMMENTS_LASTCOMMENTS', 		'Poslední komentáøe');
+define('_EZCOMMENTS_USERNAME', 			'Uživatel');
+define('_EZCOMMENTS_MODULE', 			'Modul');
+define('_EZCOMMENTS_COMMENT', 			'Komentáø');
+
+define('_EZCOMMENTS_CLEANUP_NOTHINGTODO', 'Nejsou žádné osiøelé komentáøe');
+define('_EZCOMMENTS_CLEANUP_GOBACK',      'Zpìt');
+define('_EZCOMMENTS_CLEANUP_EXPLAIN',     'Zde mùžete smazat všechny komentáøe, které v databázi osiøely po odstranìných modulech.');
+define('_EZCOMMENTS_CLEANUP_LABEL',       'Vyberte modul:');
+define('_EZCOMMENTS_CLEANUP_GO',          'Smazat všechny komentáøi k tomuto modulu');
+define('_EZCOMMENTS_CLEANUP',             'Smazat osiøelé komentáøe');
+
+define('_EZCOMMENTS_MIGRATE_EXPLAIN',     'Import komentáøù z jiných modulù');
+define('_EZCOMMENTS_MIGRATE_NOTHINGTODO', 'Plugin pro migraci není k dispozici');
+define('_EZCOMMENTS_MIGRATE_GOBACK',      'Zpìt');
+define('_EZCOMMENTS_MIGRATE_LABEL',       'Migrovat z:');
+define('_EZCOMMENTS_MIGRATE_GO',          'Zahájit migraci');
+define('_EZCOMMENTS_MIGRATE',             'Migrovat komentáøe');
+
+define('_EZCOMMENTS_FAILED1', 			'Chyba pøi vytváøení tabulky');
+define('_EZCOMMENTS_FAILED2', 			'Chyba pøi vytváøení hook');
+define('_EZCOMMENTS_FAILED3', 			'Chyba pøi mazání tabulky');
+define('_EZCOMMENTS_FAILED4', 			'Chyba pøi mazání hook');
+define('_EZCOMMENTS_FAILED5', 			'Aktualizace tabulky selhala');
+
+define('_EZCOMMENTS_MAILSUBJECT',		'Byl pøidán nový komentáø'); 
+define('_EZCOMMENTS_MAILBODY',  		'Byl pøidán nový komentáø'); 
+
+?>
