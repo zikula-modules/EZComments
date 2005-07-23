93f08bc6ed2adefb21c7eb4c8370ac04a7e77184
jna <jna@code.zikula.org>
2005-07-23 10:04:09 +0000

    synced language file with the english one

--- modules/EZComments/pnlang/deu/global.php
+++ modules/EZComments/pnlang/deu/global.php
@@ -18,30 +18,31 @@
 // To read the license please visit http://www.gnu.org/copyleft/gpl.html
 // ----------------------------------------------------------------------
 
-define('_EZCOMMENTS',         			'Kommentare');
-define('_EZCOMMENTS_NOAUTH',   			'Keine Berechtigung für die Kommentare.');
-define('_EZCOMMENTS_ONLYREG',   		'Nur angemeldete Benutzer dürfen Kommentare verfassen.');
-define('_EZCOMMENTS_GOTOREG',   		'Zur Registrierung/Anmeldung');
-define('_EZCOMMENTS_ADD', 	  			'Hinzufügen');
-define('_EZCOMMENTS_DEL', 	  			'Diesen Kommentar löschen');
-define('_EZCOMMENTS_COMMENT_ADD', 		'Einen Kommentar hinzufügen');
-define('_EZCOMMENTS_COMMENT_ANSWER', 	'Hierauf antworten');
-define('_EZCOMMENTS_COMMENT_FROM',  	'von');
-define('_EZCOMMENTS_COMMENT_ON',    	'am');
-define('_EZCCOMMENTSCREATED', 			'Kommentar hinzugefügt');
-define('_EZCCOMMENTSDELETED', 			'Kommentar gelöscht');
-define('_EZCOMMENTS_FAILED',   			'Interner Fehler');
-define('_EZCOMMENTS_NODIRECTACCESS',	'Kein direkter Zugriff auf dieses Modul');
-define('_EZCOMMENTS_RULES',				'Hier sollten die Regeln für die Benutzung der Kommentaren hinterlegt werden.');
+define('_EZCOMMENTS',                     'Kommentare');
+define('_EZCOMMENTS_NOAUTH',              'Keine Berechtigung für die Kommentare.');
+define('_EZCOMMENTS_ONLYREG',             'Nur angemeldete Benutzer dürfen Kommentare verfassen.');
+define('_EZCOMMENTS_GOTOREG',             'Zur Registrierung/Anmeldung');
+define('_EZCOMMENTS_ADD',                 'Hinzufügen');
+define('_EZCOMMENTS_DEL',                 'Diesen Kommentar löschen');
+define('_EZCOMMENTS_COMMENT_ADD',         'Einen Kommentar hinzufügen');
+define('_EZCOMMENTS_COMMENT_ANSWER',      'Hierauf antworten');
+define('_EZCOMMENTS_COMMENT_FROM',        'von');
+define('_EZCOMMENTS_COMMENT_ON',          'am');
+define('_EZCCOMMENTSCREATED',             'Kommentar hinzugefügt');
+define('_EZCCOMMENTSDELETED',             'Kommentar gelöscht');
+define('_EZCOMMENTS_FAILED',              'Interner Fehler');
+define('_EZCOMMENTS_NODIRECTACCESS',      'Kein direkter Zugriff auf dieses Modul');
+define('_EZCOMMENTS_RULES',               'Hier sollten die Regeln für die Benutzung der Kommentaren hinterlegt werden.');
 
-define('_EZCOMMENTS_ADMIN',				'EZComments Administration');
-define('_EZCOMMENTS_ADMIN_MAIN',		'Hauptmenü');
-define('_EZCOMMENTS_SENDINFOMAIL',		'Mail bei neuen Kommentaren verschicken');
-define('_EZCOMMENTS_OK', 				'Bestätigen');
-define('_EZCOMMENTS_LASTCOMMENTS', 		'Die letzten Kommentare');
-define('_EZCOMMENTS_USERNAME', 			'Benutzer');
-define('_EZCOMMENTS_MODULE', 			'Modul');
-define('_EZCOMMENTS_COMMENT', 			'Kommentar');
+define('_EZCOMMENTS_ADMIN',               'EZComments Administration');
+define('_EZCOMMENTS_ADMIN_MAIN',          'Hauptmenü');
+define('_EZCOMMENTS_SENDINFOMAIL',        'Mail bei neuen Kommentaren verschicken');
+define('_EZCOMMENTS_OK',                  'Bestätigen');
+define('_EZCOMMENTS_LASTCOMMENTS',        'Die letzten Kommentare');
+define('_EZCOMMENTS_USERNAME',            'Benutzer');
+define('_EZCOMMENTS_MODULE',              'Modul');
+define('_EZCOMMENTS_TEMPLATE',            'Layout');
+define('_EZCOMMENTS_DELETESELECTED',      'Gewählte Kommentare löschen');
 
 define('_EZCOMMENTS_CLEANUP_NOTHINGTODO', 'Es gibt keine verwaisten Kommentare');
 define('_EZCOMMENTS_CLEANUP_GOBACK',      'Zurück');
@@ -57,68 +58,80 @@ define('_EZCOMMENTS_MIGRATE_LABEL',       'Migration von:');
 define('_EZCOMMENTS_MIGRATE_GO',          'Migration starten');
 define('_EZCOMMENTS_MIGRATE',             'Kommentare migrieren');
 
-define('_EZCOMMENTS_FAILED1', 			'Erstellung der Tabelle fehlgeschlagen');
-define('_EZCOMMENTS_FAILED2', 			'Registrierung des Hooks fehlgeschlagen');
-define('_EZCOMMENTS_FAILED3', 			'Löschen der Tabelle fehlgeschlagen');
-define('_EZCOMMENTS_FAILED4', 			'Löschen des Hooks fehlgeschlagen');
-define('_EZCOMMENTS_FAILED5', 			'Update der Tabellen fehlgeschlagen');
+define('_EZCOMMENTS_FAILED1',             'Erstellung der Tabelle fehlgeschlagen');
+define('_EZCOMMENTS_FAILED2',             'Registrierung des Hooks fehlgeschlagen');
+define('_EZCOMMENTS_FAILED3',             'Löschen der Tabelle fehlgeschlagen');
+define('_EZCOMMENTS_FAILED4',             'Löschen des Hooks fehlgeschlagen');
+define('_EZCOMMENTS_FAILED5',             'Update der Tabellen fehlgeschlagen');
 
-define('_EZCOMMENTS_MAILSUBJECT',		'Neuer Kommentar'); 
-define('_EZCOMMENTS_MAILBODY',  		'Ein neuer Kommentar wurde eingegeben'); 
-// Steffen 01/2005
-define('_EZCOMMENTS_POSTED',  			'verfasst'); 
-define('_EZCOMMENTS_REG_SINCE',  		'registriert'); 
-define('_EZCOMMENTS_STATUS',  			'Status'); 
-define('_EZCOMMENTS_OFFLINE',  			'Offline'); 
-define('_EZCOMMENTS_ONLINE',  			'Online'); 
-define('_EZCOMMENTS_PROFILE',  			'Profil'); 
-define('_EZCOMMENTS_SEND_PM',  			'PM sende'); 
-define('_EZCOMMENTS_FROM',  			  'Wohnort'); 
-define('_EZCOMMENTS_SUBJECT',  			'Titel');
-define('_EZCOMMENTS_AUTHOR',  			'Autor');
+define('_EZCOMMENTS_MAILSUBJECT',         'Neuer Kommentar'); 
+define('_EZCOMMENTS_MAILBODY',            'Ein neuer Kommentar wurde eingegeben'); 
 
-// Steffen 0.8, 05/2005 
-// user
-define('_EZCOMMENTS_DISPLAY',  			'Darstellung');
-define('_EZCOMMENTS_FLAT',  			  'Flach');
-define('_EZCOMMENTS_THREADED',  		'Kommentarbaum');
-define('_EZCOMMENTS_OLDESTFIRST',  	'absteigend');
-define('_EZCOMMENTS_NEWESTFIRST',  	'aufsteigend');
-define('_EZCOMMENTS_ORDER',  			  'Reihenfolge');
+define('_EZCOMMENTS_POSTED',              'verfasst'); 
+define('_EZCOMMENTS_REG_SINCE',           'registriert'); 
+define('_EZCOMMENTS_STATUS',              'Status'); 
+define('_EZCOMMENTS_OFFLINE',             'Offline'); 
+define('_EZCOMMENTS_ONLINE',              'Online'); 
+define('_EZCOMMENTS_PROFILE',             'Profil'); 
+define('_EZCOMMENTS_SEND_PM',             'PM senden'); 
+define('_EZCOMMENTS_FROM',                'Wohnort'); 
+define('_EZCOMMENTS_SUBJECT',             'Titel');
+define('_EZCOMMENTS_AUTHOR',              'Autor');
 
-// admin
-define('_EZCOMMENTS_MISCSETTINGS',  'Einstellungen');
-define('_EZCOMMENTS_ITEMSPERPAGE',  'Kommentar pro Seite');
-define('_EZCOMMENTS_TEMPLATE',  		'Layout');
-define('_EZCOMMENTS_ALLOWANONUSERSETINFO',  'Anonyme User dürfen ihre Daten angeben');
-define('_EZCOMMENTS_NOTIFICATIONSETTINGS',  'Benachrichtigungen');
-define('_EZCOMMENTS_SENDINFOMAILMOD',			  'Bitten um Moderation mailen');
-define('_EZCOMMENTS_MODERATIONSETTINGS',  	'Moderation');
-define('_EZCOMMENTS_MODERERATE',  	'Moderierte Kommentare');
-define('_EZCOMMENTS_ALWAYSMODERERATE',  		'Nur moderiert freigeben');
-define('_EZCOMMENTS_MODLINKCOUNT',  'Höchstanzahl von Links, bevor freigeschaltet werden muss');
-define('_EZCOMMENTS_MODLIST',  			'Wörter, die automatisch Moderation erfordern');
-define('_EZCOMMENTS_SEPERATELINE',  'Ein Stichwort pro Zeile');
-define('_EZCOMMENTS_BLACKLIST',  		'Wörter die zur Löschung führen');
-define('_EZCOMMENTS_BLACKLISTNOTE', 'Achtung: Kommentare, die diese Wörter enthalten, werden nicht zugelassen');
-define('_EZCOMMENTS_MODMAILSUBJECT',       'Neuer Kommentar');
-define('_EZCOMMENTS_MODMAILBODY',          'Ein neuer Kommentar wurde erstellt und erfordert Moderation.');
-define('_EZCOMMENTS_HELDFORMODERATION',    'Der Kommentar wird so bald wie möglich freigeschaltet. Vielen Dank');
-define('_EZCOMMENTS_COMMENTBLACKLISTED',   'Der Kommentar wurde auf grund inhaltlicher Mängel nicht freigeschaltet');
+define('_EZCOMMENTS_EDIT',                'Kommentar bearbeiten');
+define('_EZCOMMENTS_ITEMSPERPAGE',        'Kommentare pro Seite');
+define('_EZCOMMENTS_COMMENT',             'Kommentar');
 
-define('_EZCOMMENTS_SEARCH',               'Kommentare durchsuchen');
-define('_EZCOMMENTS_NOCOMMENTSFOUND',      'Keine passenden Kommentare gefunden');
+define('_EZCOMMENTS_DISPLAY',             'Darstellung');
+define('_EZCOMMENTS_FLAT',                'Flach');
+define('_EZCOMMENTS_THREADED',            'Kommentarbaum');
+define('_EZCOMMENTS_OLDESTFIRST',         'absteigend');
+define('_EZCOMMENTS_NEWESTFIRST',         'aufsteigend');
+define('_EZCOMMENTS_ORDER',               'Reihenfolge');
 
-define('_EZCOMMENTS_ANONNAME',             'Name');
-define('_EZCOMMENTS_ANONMAIL',             'E-mail Adresse');
+define('_EZCOMMENTS_ALLOWANONUSERSETINFO', 'Anonyme User dürfen ihre Daten angeben');
+define('_EZCOMMENTS_ANONNAME',            'Name');
+define('_EZCOMMENTS_ANONMAIL',            'E-mail Adresse');
+
+define('_EZCOMMENTS_SEARCH',              'Kommentare durchsuchen');
+define('_EZCOMMENTS_NOCOMMENTSFOUND',     'Keine passenden Kommentare gefunden');
+
+define('_EZCOMMENTS_TOP',                 'Top');
+define('_EZCOMMENTS_BOTTOM',              'Bottom');
+
+// comment moderation
+define('_EZCOMMENTS_MODERERATE',          'Moderierte Kommentare');
+define('_EZCOMMENTS_MODLINKCOUNT',        'Höchstanzahl von Links, bevor freigeschaltet werden muss');
+define('_EZCOMMENTS_MODLIST',             'Wörter, die automatisch Moderation erfordern');
+define('_EZCOMMENTS_BLACKLIST',           'Wörter die zur Löschung führen');
+define('_EZCOMMENTS_BLACKLISTNOTE',       'Achtung: Kommentare, die diese Wörter enthalten, werden nicht zugelassen');
+define('_EZCOMMENTS_SEPERATELINE',        'Ein Stichwort pro Zeile');
+define('_EZCOMMENTS_SENDINFOMAILMOD',     'Bitten um Moderation mailen');
+define('_EZCOMMENTS_MODMAILSUBJECT',      'Neuer Kommentar');
+define('_EZCOMMENTS_MODMAILBODY',         'Ein neuer Kommentar wurde erstellt und erfordert Moderation.');
+define('_EZCOMMENTS_ALWAYSMODERERATE',    'Nur moderiert freigeben');
+define('_EZCOMMENTS_HELDFORMODERATION',   'Der Kommentar wird so bald wie möglich freigeschaltet. Vielen Dank');
+define('_EZCOMMENTS_COMMENTBLACKLISTED',  'Der Kommentar wurde aufgrund inhaltlicher Mängel nicht freigeschaltet');
+define('_EZCOMMENTS_PROXYBLACKLIST',      'Blacklist comments from insecure proxies');
 
 // comment statuses
-define('_EZCOMMENTS_APPROVED', 'freigeschaltet');
-define('_EZCOMMENTS_PENDING', 'wartet');
-define('_EZCOMMENTS_REJECTED', 'abgelehnt');
+define('_EZCOMMENTS_APPROVED',            'freigeschaltet');
+define('_EZCOMMENTS_PENDING',             'wartet');
+define('_EZCOMMENTS_REJECTED',            'abgelehnt');
+
+// modifyconfig fieldsets
+define('_EZCOMMENTS_MISCSETTINGS',        'Einstellungen');
+define('_EZCOMMENTS_NOTIFICATIONSETTINGS','Benachrichtigungen');
+define('_EZCOMMENTS_MODERATIONSETTINGS',  'Moderation');
 
 // mails
-define('_EZCOMMENTS_SHOW', 'Anzeigen');
-define('_EZCOMMENTS_MODERATE2', 'Freigeben/Ablehnen');
-define('_EZCOMMENTS_DELETE', 'Löschen');
+define('_EZCOMMENTS_SHOW',                'Anzeigen');
+define('_EZCOMMENTS_MODERATE2',           'Freigeben/Ablehnen');
+define('_EZCOMMENTS_DELETE',              'Löschen');
+
+// comment purging options
+define('_EZCOMMENTS_PURGE',               'Kommentare entfernen');
+define('_EZCOMMENTS_PURGEPENDING',        'Alle noch nicht freigegebenen Kommentare entfernen');
+define('_EZCOMMENTS_PURGEREJECTED',       'Alle zurückgewiesenen Kommentare entfernen');
+
 ?>
