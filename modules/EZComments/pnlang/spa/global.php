90510d34da7f3516c6b43c72cac41bae34909d84
jna <jna@code.zikula.org>
2005-10-18 04:16:34 +0000

    updated Spanish translations from Imoq

--- modules/EZComments/pnlang/spa/global.php
+++ modules/EZComments/pnlang/spa/global.php
@@ -42,7 +42,7 @@ define('_EZCOMMENTS_LASTCOMMENTS',          'Los últimos comentarios');
 define('_EZCOMMENTS_USERNAME',              'Usuario');
 define('_EZCOMMENTS_MODULE',                'Módulo');
 define('_EZCOMMENTS_TEMPLATE',              'Plantilla predeterminada');
-define('_EZCOMMENTS_DELETESELECTED',        'Delete selected comments');
+define('_EZCOMMENTS_DELETESELECTED',        'Borrar comentarios seleccionados');
 
 define('_EZCOMMENTS_CLEANUP_NOTHINGTODO',   'No hay comentarios huérfanos');
 define('_EZCOMMENTS_CLEANUP_GOBACK',        'Regresar');
@@ -66,10 +66,9 @@ define('_EZCOMMENTS_FAILED5',               'Falló la actualización de la Tabla'
 
 define('_EZCOMMENTS_MAILSUBJECT',           'Se recibió un nuevo comentario');
 define('_EZCOMMENTS_MAILBODY',              'Se recibió un nuevo comentario');
-
 define('_EZCOMMENTS_POSTED',                'escrito');
 define('_EZCOMMENTS_REG_SINCE',             'registrado');
-define('_EZCOMMENTS_NOTREG',                'non registrado'); 
+define('_EZCOMMENTS_NOTREG',                'no registrado'); 
 define('_EZCOMMENTS_STATUS',                'Estado');
 define('_EZCOMMENTS_OFFLINE',               'Desconectado');
 define('_EZCOMMENTS_ONLINE',                'Conectado');
@@ -116,6 +115,7 @@ define('_EZCOMMENTS_ALWAYSMODERERATE',      'Todos los comentarios requieren ser
 define('_EZCOMMENTS_HELDFORMODERATION',     'Tu comentario se ha retenido par ser moderado y será revisado pronto');
 define('_EZCOMMENTS_COMMENTBLACKLISTED',    'Tu comentario contiene contenido inaceptable y ha sido rechazado');
 define('_EZCOMMENTS_PROXYBLACKLIST',        'Rechazar comentarios de proxies inseguros');
+define('_EZCOMMENTS_DONTMODERATEIFCOMMENTED', 'No requerir moderación para comentarios de usuarios que han comentado antes');
 
 // comment statuses
 define('_EZCOMMENTS_APPROVED',              'Aprobado');
@@ -128,21 +128,62 @@ define('_EZCOMMENTS_MODERATIONSETTINGS',    'Moderación');
 define('_EZCOMMENTS_NOTIFICATIONSETTINGS',  'Notificación');
 
 // mails
-define('_EZCOMMENTS_SHOW',                  'Show');
-define('_EZCOMMENTS_MODERATE2',             'Moderate');
-define('_EZCOMMENTS_DELETE',                'Delete');
+define('_EZCOMMENTS_SHOW',                  'Mostrar');
+define('_EZCOMMENTS_MODERATE2',             'Moderar');
+define('_EZCOMMENTS_DELETE',                'Eliminar');
 
 // comment purging options
-define('_EZCOMMENTS_PURGE',                 'Purge comments');
-define('_EZCOMMENTS_PURGEPENDING',          'Purge all pending comments');
-define('_EZCOMMENTS_PURGEREJECTED',         'Purge all rejected comments');
+define('_EZCOMMENTS_PURGE',                 'Purgar comentarios');
+define('_EZCOMMENTS_PURGEPENDING',          'Purgar todos los comentarios pendientes');
+define('_EZCOMMENTS_PURGEREJECTED',         'Purgar todos los comentarios rechazados');
 
 // Block
 define('_EZCOMMENTS_NUMENTRIES',            'Número de comentarios a mostrar');
 define('_EZCOMMENTS_SHOWUSERNAME',          '¿Mostrar nombre de usuario?');
 define('_EZCOMMENTS_LINKUSERNAME',          '¿Enlazar nombre de usuario al perfil?');
 define('_EZCOMMENTS_SHOWDATE',              '¿Mostrar fecha?');
-define('_EZCOMMENTS_SELECT_MODULE',         'Show comments for the following module');
-define('_EZCOMMENTS_ALLMODULES',            'All');
+define('_EZCOMMENTS_SELECT_MODULE',         'Mostrar comentarios para el módulo siguiente');
+define('_EZCOMMENTS_ALLMODULES',            'Todos');
+
+// ip address logging
+define('_EZCOMMENTS_IPADDR',                'Dirección IP');
+define('_EZCOMMENTS_LOGIPADDR',             'Grabar direcciones IP');
+define('_EZCOMMENTS_IPADDRNOTLOGGED',       'Dirección IP no grabada');
+
+// multiple comment processing
+define('_EZCOMMENTSWITHSELECTED',            'Con los comentarios seleccionados: ');
+define('_EZCOMMENTS_APPROVE',                'Aprobar');
+define('_EZCOMMENTS_REJECT',                 'Rechazar');
+define('_EZCOMMENTS_HOLD',                   'Esperar');
+
+// comment stats
+define('_EZCOMMENTS_STATS',                  'Estadísticas de comentarios');
+define('_EZCOMMENTS_TOTAL',                  'Comentarios totales');
+define('_EZCOMMENTS_ITEM',                   'ID del Item');
+define('_EZCOMMENTS_CONFIRMDELETEMODULE',    'Confirma el borrar todos los comentarios del módulo \'%m%\'');
+define('_EZCOMMENTS_CANCELDELETEMODULE',     'Cancelar el borrado de todos los comentarios del módulo \'%m%\'');
+define('_EZCOMMENTS_CONFIRMDELETEITEM',      'Confirmar el borrado de todos los comentarios del objeto \'%o%\' en el módulo \'%m%\'');
+define('_EZCOMMENTS_CANCELDELETEITEM',       'Cancelar el borrado de todos los comentarios del objeto \'%o%\' en el módulo \'%m%\'');
+
+// comment typing
+define('_EZCOMMENTS_TYPE',                    'Tipo de comentario');
+
+// comment feeds
+define('_EZCOMMENTS_FEEDS',                       'Feeds');
+define('_EZCOMMENTS_FEEDTYPE',                'Tipo de feed');
+define('_EZCOMMENTS_FEEDCOUNT',               'Número de elementos a mostrar en el feed');
+define('_EZCOMMENTS_ATOM',                    'Atom 0.3');
+define('_EZCOMMENTS_RSS',                     'RSS 2.0');
+define('_EZCOMMENTS_FEEDNOTE',                'Nota: Tanto el tipo de feed y el número de feeds pueden ser sobreescritos usando los parámetros feedtype y feedcount añadido al URL del feed');
+
+// some other users phrases in comment templates
+define('_EZCOMMENTS_COMMENTSOFAR',            'Comentarios hasta ahora');
+define('_EZCOMMENTS_WROTEON',                 'escrito el');
+define('_EZCOMMENTS_AT',                      'a las');
+define('_EZCOMMENTS_LEAVEACOMMENT',           'Deja un comentario');
+
+// pager defines
+define('_EZCOMMENTS_ENABLEPAGER',             'Habilitar paginador (vista de usuario)');
+define('_EZCOMMENTS_COMMENTSPERPAGE',         'Comentarios por página (vista de usuario)');
 
 ?>
