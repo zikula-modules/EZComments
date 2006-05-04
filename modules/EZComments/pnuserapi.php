d226e4b2447c0c7142eab11706c5562148dc6392
Mark West <mark@markwest.me.uk>
2006-05-04 08:36:25 +0000

    added base url to mail templates [markwest]

--- modules/EZComments/pnuserapi.php
+++ modules/EZComments/pnuserapi.php
@@ -370,6 +370,7 @@ function EZComments_userapi_create($args)
         $pnRender->assign('url', $url);
         $pnRender->assign('moderate', pnModURL('EZComments', 'admin', 'modify', array('id' => $id)));
         $pnRender->assign('delete', pnModURL('EZComments', 'admin', 'delete', array('id' => $id)));
+        $pnRender->assign('baseURL', pnGetBaseURL());
         $mailsubject = _EZCOMMENTS_MAILSUBJECT;
         $mailbody = $pnRender->fetch('ezcomments_mail_newcomment.htm');
         pnModAPIFunc('Mailer', 'user', 'sendmessage',
@@ -384,6 +385,7 @@ function EZComments_userapi_create($args)
         $pnRender->assign('url', $url);
         $pnRender->assign('moderate', pnModURL('EZComments', 'admin', 'modify', array('id' => $id)));
         $pnRender->assign('delete', pnModURL('EZComments', 'admin', 'delete', array('id' => $id)));
+        $pnRender->assign('baseURL', pnGetBaseURL());
         $mailsubject = _EZCOMMENTS_MODMAILSUBJECT;
         $mailbody = $pnRender->fetch('ezcomments_mail_modcomment.htm');
         pnModAPIFunc('Mailer', 'user', 'sendmessage',
