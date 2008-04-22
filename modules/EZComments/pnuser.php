267096a877a32d12f18ebe64436a208e6e5312e5
Florian Schießl <florian.schiessl@ifs-net.de>
2008-04-22 20:51:39 +0000

    The sort order can now be set as parameter when you call the hook function. sortorder="DESC" => descending and everything else: ASC...

--- modules/EZComments/pnuser.php
+++ modules/EZComments/pnuser.php
@@ -150,9 +150,10 @@ function EZComments_user_view($args)
     }
     
     // if there is no sortorder set yet we'l take the $args['sortorder'] parameter
-    if (!isset($sortorder) && isset($args['sortorder']))
-    if (strtolower($args['sortorder']) == 'desc') $sortorder = 'DESC';
-    else $sortorder = 'ASC';
+    if (!isset($sortorder) && isset($args['sortorder'])) {
+	  if (strtolower($args['sortorder']) == 'desc') $sortorder = 'DESC';
+	    else $sortorder = 'ASC';
+	}
     
     $status = 0;
 
