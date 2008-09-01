d30e1b3e5a4392987e800ee5917fdfb9f02a56b0
Florian Schießl <florian.schiessl@ifs-net.de>
2008-09-01 20:17:37 +0000

    myprofile plugin interface changes

--- modules/EZComments/pnmyprofileapi.php
+++ modules/EZComments/pnmyprofileapi.php
@@ -46,11 +46,11 @@ function EZComments_myprofileapi_getTitle($args)
 /**
  * This function returns additional options that should be added to the plugin url
  *
- * @return string
+ * @return array
  */
 function EZComments_myprofileapi_getURLAddOn($args)
 {
-  	return 'order=1';
+  	return array('order'=>1);
 }
 
 /**
