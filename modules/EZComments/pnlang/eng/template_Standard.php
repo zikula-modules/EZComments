d174c6ba69238d989e9bc1633bb5cfd4f4b98fbd
Florian Schießl <florian.schiessl@ifs-net.de>
2009-02-15 16:24:05 +0000

    adding language file for new standard template

---
+++ modules/EZComments/pnlang/eng/template_Standard.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * $Id: $
+ * 
+ * * EZComments *
+ * 
+ * Attach comments to any module calling hooks
+ * 
+ * 
+ * * License *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * @author Joerg Napp <jnapp@users.sourceforge.net>
+ * @author Mark West <markwest at zikula dot org>
+ * @author Jean-Michel Vedrine
+ * @author Florian Schieﬂl <florian.schiessl at ifs-net.de>
+ * @author Frank Schummertz
+ * @version 1.6
+ * @link http://code.zikula.org/ezcomments/ Support and documentation
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @package Zikula_3rdParty_Modules
+ * @subpackage EZComments
+ */
+
+define('_EZCOMMENTS_AUTHOR',		'Author');
+define('_EZCOMMENTS_COMMENTS',		'Comments');
+define('_EZCOMMENTS_SENDPM',		'Send mail to user');
+define('_EZCOMMENTS_NOCOMMENTSYET',	'No comments posted yet');
