4d78b9e540954a35e937764b09ddae0cfb0a3032
Carsten Volmer <carsten@zikula.org>
2010-06-09 06:45:00 +0000

    copyright problems

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -1,49 +1,9 @@
-/* javascript code taken from dustiandiaz.com article entitled Check one, check all Javascript
-Reference: http://www.dustindiaz.com/archives/2005/04/check_one_check_all_javascript.php */
-function checkAllFields(ref)
-{
-    var chkAll = document.getElementById('checkAll');
-    var checks = document.getElementsByName('comments[]');
-    var removeButton = document.getElementById('removeChecked');
-    var boxLength = checks.length;
-    var allChecked = false;
-    var totalChecked = 0;
-    if ( ref == 1 )
-    {
-        if ( chkAll.checked == true )
-        {
-            for ( i=0; i < boxLength; i++ )
-            checks[i].checked = true;
-        }
-        else
-        {
-            for ( i=0; i < boxLength; i++ )
-            checks[i].checked = false;
-        }
-    }
-    else
-    {
-        for ( i=0; i < boxLength; i++ )
-        {
-            if ( checks[i].checked == true )
-            {
-                allChecked = true;
-                continue;
-            }
-            else
-            {
-                allChecked = false;
-                break;
-            }
-        }
-        if ( allChecked == true )
-        chkAll.checked = true;
-        else
-        chkAll.checked = false;
-    }
-    for ( j=0; j < boxLength; j++ )
-    {
-        if ( checks[j].checked == true )
-        totalChecked++;
-    }
+function CheckAll(formtype) {
+    $$('.' + formtype + '_checkbox').each(function(el) { el.checked = $('toggle_' + formtype).checked;});
+}
+
+function CheckCheckAll(formtype) {
+    var totalon = 0;
+    $$('.' + formtype + '_checkbox').each(function(el) { if (el.checked) { totalon++; } });
+    $('toggle_' + formtype).checked = ($$('.' + formtype + '_checkbox').length==totalon);
 }
