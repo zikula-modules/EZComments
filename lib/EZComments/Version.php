ba1bad0ecaebc48f9f29e25a17fbd2b32d6c21ce
Drak <drak@zikula.org>
2010-07-06 12:58:45 +0000

    Refactor.

--- lib/EZComments/Version.php
+++ lib/EZComments/Version.php
@@ -7,14 +7,14 @@
  * @license See license.txt
  */
 
-class Content_Version extends Zikula_Version
+class EZComments_Version extends Zikula_Version
 {
     public function getMetaData()
     {
         $meta = array();
         // Information for the modules admin
-        $meta['displayname']    = __('Comments', $dom);
-        $meta['description']    = __('Attach comments to every kind of content using hooks', $dom);
+        $meta['displayname']    = __('Comments');
+        $meta['description']    = __('Attach comments to every kind of content using hooks');
         //! module url in lowercase and different to displayname
         $meta['url']            = __('comments', $dom);
         $meta['version']        = '2.1.0';
@@ -29,16 +29,16 @@ class Content_Version extends Zikula_Version
         $meta['dependencies'] = array(
                 array('modname'    => 'akismet',
                         'minversion' => '1.0', 'maxversion' => '',
-                        'status'     => PNMODULE_DEPENDENCY_RECOMMENDED),
+                        'status'     => ModUtil::DEPENDENCY_RECOMMENDED),
                 array('modname'    => 'ContactList',
                         'minversion' => '1.0', 'maxversion' => '',
-                        'status'     => PNMODULE_DEPENDENCY_RECOMMENDED),
+                        'status'     => ModUtil::DEPENDENCY_RECOMMENDED),
                 array('modname'    => 'MyProfile',
                         'minversion' => '1.2', 'maxversion' => '',
-                        'status'     => PNMODULE_DEPENDENCY_RECOMMENDED),
+                        'status'     => ModUtil::DEPENDENCY_RECOMMENDED),
                 array('modname'    => 'InterCom',
                         'minversion' => '2.1', 'maxversion' => '',
-                        'status'     => PNMODULE_DEPENDENCY_RECOMMENDED)
+                        'status'     => ModUtil::DEPENDENCY_RECOMMENDED)
         );
         return $meta;
     }
