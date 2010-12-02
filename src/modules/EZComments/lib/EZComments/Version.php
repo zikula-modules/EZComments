d71f6d69b1797a2dc1b7849dbc73f15e8d8aabb6
Drak <drak@zikula.org>
2010-12-02 18:43:11 +0545

    Fix up filter hooks.

--- src/modules/EZComments/lib/EZComments/Version.php
+++ src/modules/EZComments/lib/EZComments/Version.php
@@ -9,37 +9,45 @@
 
 class EZComments_Version extends Zikula_Version
 {
+
     public function getMetaData()
     {
         $meta = array();
         // Information for the modules admin
-        $meta['displayname']    = $this->__('Comments');
-        $meta['description']    = $this->__('Attach comments to every kind of content using hooks');
+        $meta['displayname'] = $this->__('Comments');
+        $meta['description'] = $this->__('Attach comments to every kind of content using hooks');
         //! module url in lowercase and different to displayname
-        $meta['url']            = $this->__('comments');
-        $meta['version']        = '3.0.1';
+        $meta['url'] = $this->__('comments');
+        $meta['version'] = '3.0.1';
         $meta['securityschema'] = array(
-                'EZComments::'          => 'Module:Item ID:Comment ID',
+                'EZComments::' => 'Module:Item ID:Comment ID',
                 'EZComments::trackback' => 'Module:Item ID:',
-                'EZComments::pingback'  => 'Module:Item ID:'
+                'EZComments::pingback' => 'Module:Item ID:'
         );
 
-        $meta['capabilities'] = array(HookUtil::PROVIDER_CAPABLE => array('enabled' => true));
+        $meta['capabilities'] = array();
+        $meta['capabilities'][HookUtil::PROVIDER_CAPABLE] = array('enabled' => true);
+        $meta['capabilities'][HookUtil::SUBSCRIBER_CAPABLE] = array('enabled' => true);
 
         // recommended and required modules
         $meta['dependencies'] = array(
                 array('modname'    => 'Akismet',
                         'minversion' => '1.0', 'maxversion' => '',
-                        'status'     => ModUtil::DEPENDENCY_RECOMMENDED)
+                        'status' => ModUtil::DEPENDENCY_RECOMMENDED)
         );
         return $meta;
     }
 
     protected function setupHookBundles()
     {
-         $bundle = new Zikula_Version_HookProviderBundle('modulehook_area.ezcomments.comments', __('EZComments Comment Handlers'));
-         $bundle->addHook('hookhandler.ezcomments.ui.view', 'ui.view', 'EZComments_HookHandlers', 'ui_view', 'ezcomments.hooks');
-         $bundle->addHook('hookhandler.ezcomments.process.delete', 'process.delete', 'EZComments_HookHandlers', 'process_delete', 'ezcomments.hooks');
-         $this->registerHookProviderBundle($bundle);
+        $bundle = new Zikula_Version_HookProviderBundle('modulehook_area.ezcomments.comments', __('EZComments Comment Hooks'));
+        $bundle->addHook('hookhandler.ezcomments.ui.view', 'ui.view', 'EZComments_HookHandlers', 'ui_view', 'ezcomments.hooks');
+        $bundle->addHook('hookhandler.ezcomments.process.delete', 'process.delete', 'EZComments_HookHandlers', 'process_delete', 'ezcomments.hooks');
+        $this->registerHookProviderBundle($bundle);
+
+        $bundle = new Zikula_Version_HookSubscriberBundle('modulehook_area.ezcomments.commentsfilter', $this->__('EZComment Comments Filter'));
+        $bundle->addType('ui.filter', 'ezcomments.hook.commentsfilter');
+        $this->registerHookSubscriberBundle($bundle);
     }
+
 }
