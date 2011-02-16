aa7e90a8bf01f9befe9d3bbf206646121bc290a3
Craig Heydenburg <craigh@mac.com>
2011-02-16 17:57:23 -0500

    avoid potential E_ALL error. fixes #95. credit phaidon.

--- src/modules/EZComments/lib/EZComments/Api/Account.php
+++ src/modules/EZComments/lib/EZComments/Api/Account.php
@@ -17,10 +17,10 @@ class EZComments_Api_Account extends Zikula_Api
     */
     public function getall()
     {
+        $items = array();
         $useAccountPage = ModUtil::getVar('EZComments', 'useaccountpage', '1');
         if ($useAccountPage) {
             // Create an array of links to return
-            $items = array();
             $items['1'] = array('url'   => ModUtil::url('EZComments', 'user', 'main'),
                                 'title' => $this->__('Manage my comments'),
                                 'icon'  => 'mycommentsbutton.gif',
